4488b684077cfb68b5ec37a84aa9961acb00933a
Brian Paak <4719835+bhpaak@users.noreply.github.com>
2023-09-26 17:08:53 -0700

    Enable button to set grid size w/o reloading page

--- javascript.js
+++ javascript.js
@@ -1,29 +1,54 @@
-let container = document.querySelector('.container');
+let sketchpad = document.querySelector('.sketchpad');
 
 // const containerWidth = 600;
-const containerHeight = 600; 
+const sketchpadHeight = 600; 
 
-for (i = 1; i <= 16; i++) {
-    let divRow = document.createElement('div');
-    divRow.classList.add('divRow');
-    divRow.setAttribute('style', `height: ${containerHeight/16}px;`)
-    for (j = 1; j <= 16; j++) {
-        let divColumn = document.createElement('div');
-        divColumn.classList.add('divColumn');
-        divRow.appendChild(divColumn);
+let sizeButton = document.querySelector("#set-size");
+let size; 
+let squares;
+sizeButton.addEventListener("click", (e) => {
+    if (document.querySelectorAll(".divRow")) {
+        let divRows = document.querySelectorAll(".divRow");
+        divRows.forEach( (divRow) => {
+            divRow.remove();
+        })
     }
-    container.appendChild(divRow);
-}
+    size = prompt('What size square do you want?');
+    for (i = 1; i <= size; i++) {
+        let divRow = document.createElement('div');
+        divRow.classList.add('divRow');
+        divRow.setAttribute('style', `height: ${sketchpadHeight/size}px;`)
+        for (j = 1; j <= size; j++) {
+            let divColumn = document.createElement('div');
+            divColumn.classList.add('divColumn');
+            divRow.appendChild(divColumn);
+        }
+        sketchpad.appendChild(divRow);
+    }
 
-let squares = document.querySelectorAll(".divColumn");
-squares.forEach( (square) => {
-    square.addEventListener('pointerover', changeColor);
+    squares = document.querySelectorAll(".divColumn");
+    squares.forEach( (square) => {
+        square.addEventListener('pointerover', changeColor);
+    })
+    squares.forEach ( (square) => {
+        square.addEventListener('mousedown', (e) => {
+            let divSquare = e.target;
+            divSquare.setAttribute('style', 'background-color: black;');
+        })
+    })
 })
+
+console.log(squares);
+
 
 function changeColor(e) {
     let divSquare = e.target;
-    divSquare.setAttribute('style', 'background-color: black;');
+    if (e.buttons === 1) {
+        divSquare.setAttribute('style', 'background-color: black;');
+    }
 }
+
+
 
 let button = document.querySelector('#reset');
 button.addEventListener('click', reset);
@@ -31,6 +56,6 @@ button.addEventListener('click', reset);
 function reset() {
     let divSquares = document.querySelectorAll("div.divColumn");
     divSquares.forEach( (divSquare) => {
-        divSquare.setAttribute('style', 'background-color: lightgray;')
+        divSquare.setAttribute('style', 'background-color: white;')
     })
 }
